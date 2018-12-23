6cd43e9be022720af65d4eee2f7bb555e9242422
Vladislav Nevzorov <kepka4242@gmail.com>
2018-12-23 13:45:41 +0700

    Добавьте файлы проекта.

---
+++ LedEffects/LedEffects.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		LedEffects.ino
+ Created:	23.12.2018 13:45:34
+ Author:	KepKa
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
